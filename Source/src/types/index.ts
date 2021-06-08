64e3bb1721fc345721f6c345107876ba22f8af46
alstjr7375 <alstjr7375@daum.net>
2021-06-08 15:13:07 +0900

    Add typescript code

---
+++ Source/src/types/index.ts
@@ -0,0 +1,36 @@
+// Global
+export interface resourceI {
+    oriBg: string;
+    el: HTMLElement;
+}
+
+// Mouse
+export type is_pressedI = [boolean]; // For reference variable
+
+export interface AreaI {
+    left:   number;
+    right:  number;
+    top:    number;
+    bottom: number;
+}
+
+// Effect
+export interface effectOptionsI {
+    lightColor:   string;
+    gradientSize: number;
+    clickEffect: boolean;
+    isContainer: boolean;
+    children: {
+        borderSelector:  string,
+        elementSelector: string,
+        lightColor:      string,
+        gradientSize:    number
+    };
+}
+export type userEffectOptionsI = Partial<effectOptionsI>;
+
+export interface enableEffectFuncI {
+    (element:    resourceI,
+     options:    effectOptionsI,
+     is_pressed: is_pressedI    ): void
+}
